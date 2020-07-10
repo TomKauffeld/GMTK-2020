85ae790569b33ede87136126ca9f977ba486ed15
TomKauffeld <tom.kauffeld@gmail.com>
2020-07-10 20:29:31 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2020': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 11,
+		'sourceType': 'module'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log("Hello World from your main file!");
+console.log('Hello World from your main file!');

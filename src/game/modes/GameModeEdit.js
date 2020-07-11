f8cb265ada00d1e307a7ced35ffbbed48fc5f38f
TomKauffeld <tom.kauffeld@gmail.com>
2020-07-11 10:02:54 +0200

    editor

--- src/game/modes/GameModeEdit.js
+++ src/game/modes/GameModeEdit.js
@@ -23,13 +23,52 @@ class Table
     {
         if (x >= 0 && y >= 0)
         {
-            if (typeof this.tiles[x] === 'undefined')
+            if (tile !== null)
             {
-                this.tiles[x] = {};
+                if (typeof this.tiles[x] === 'undefined')
+                {
+                    this.tiles[x] = {};
+                }
+                this.tiles[x][y] = `${tile.type}:${tile.name}`;
             }
-            this.tiles[x][y] = `${tile.type}:${tile.name}`;
+            else
+            {
+                if (typeof this.tiles[x] !== 'undefined')
+                {
+                    if (typeof this.tiles[x][y] !== 'undefined')
+                    {
+                        this.tiles[x][y] = undefined;
+                        delete this.tiles[x][y];
+                    }
+                    const keys = Object.keys(this.tiles[x]);
+                    if (keys.length < 1)
+                    {
+                        this.tiles[x] = undefined;
+                        delete this.tiles[x];
+                    }
+                }
+            }
         }
     }
+    /**
+     * 
+     * @param {number} y
+     * @param {number} xa 
+     * @returns {string|null}
+     */
+    get(y, x)
+    {
+        if (typeof this.tiles[x] === 'undefined')
+        {
+            return null;
+        }
+        if (typeof this.tiles[x][y] === 'undefined')
+        {
+            return null;
+        }
+        return this.tiles[x][y];
+    }
+
     getColumnCount()
     {
         const keysX = Object.keys(this.tiles);
@@ -44,6 +83,7 @@ class Table
         }
         return max + 1;
     }
+
     getRowCount()
     {
         const keysX = Object.keys(this.tiles);
@@ -62,6 +102,30 @@ class Table
         }
         return max + 1;
     }
+
+    export()
+    {
+        const maxX = this.getColumnCount();
+        const maxY = this.getRowCount();
+        let e = '';
+        for(let y = 0; y < maxY; y++)
+        {
+            for (let x = 0; x < maxX; x++)
+            {
+                if (x > 0)
+                {
+                    e += ',';
+                }
+                const tile = this.get(y, x);
+                if (tile !== null)
+                {
+                    e += tile;
+                }
+            }
+            e += '\r\n';
+        }
+        return e;
+    }
 }
 
 class GameModeEdit extends GameMode
@@ -74,10 +138,12 @@ class GameModeEdit extends GameMode
         this.pos = {x: 5, y: 5};
         this.types = Object.keys(Tile.collection);
         this.selectedType = 0;
+        this.speed = 4;
         this.x = 0;
         this.scale = 0;
         this.lastClick = false;
         this.selectedBlock = 0;
+        this.offset = {x: 0, y: 0};
     }
 
     /**
@@ -120,17 +186,33 @@ class GameModeEdit extends GameMode
         {
             if (this.isPressed(sketch) && this.scale > 0)
             {
-                if (!this.lastClick)
+                if (sketch.mouseX > this.x)
                 {
-                    if (sketch.mouseX > this.x)
+                    if (!this.lastClick)
                     {
-                        if (sketch.mouseY < 10)
+                        if (sketch.mouseY > 20 && sketch.mouseY < 30)
                         {
                             this.selectedType++;
                             this.selectedType %= this.types.length;
-                            if (this.selectedBlock >= Object.keys(Tile.collection[this.types[this.selectedType]]).length)
+                            if (this.selectedBlock > Object.keys(Tile.collection[this.types[this.selectedType]]).length)
                             {
                                 this.selectedBlock = 0;
+                            }
+                        }
+                        else if (sketch.mouseY < 10)
+                        {
+                            const data = this.table.export();
+                            var blob = new Blob([data], {type: 'text/csv'});
+                            if(window.navigator.msSaveOrOpenBlob) {
+                                window.navigator.msSaveBlob(blob, 'world.csv');
+                            }
+                            else{
+                                var elem = window.document.createElement('a');
+                                elem.href = window.URL.createObjectURL(blob);
+                                elem.download = 'world.csv';        
+                                document.body.appendChild(elem);
+                                elem.click();        
+                                document.body.removeChild(elem);
                             }
                         }
                         else
@@ -141,12 +223,26 @@ class GameModeEdit extends GameMode
                             if (x >= 0 && y >= 0 && y < Math.floor(sketch.height / this.scale) - 2)
                             {
                                 const i = y + x * maxY;
-                                if (i < Object.keys(Tile.collection[this.types[this.selectedType]]).length)
+                                if (i <= Object.keys(Tile.collection[this.types[this.selectedType]]).length)
                                 {
                                     this.selectedBlock = i;
                                 }
                             }
                         }
+                    }
+                }
+                else
+                {
+                    const x = Math.floor(sketch.mouseX / this.scale - this.offset.x);
+                    const y = Math.floor(sketch.mouseY / this.scale - this.offset.y);
+                    const keys = Object.keys(Tile.collection[this.types[this.selectedType]]);
+                    if (this.selectedBlock < keys.length)
+                    {
+                        this.table.set(x, y, Tile.collection[this.types[this.selectedType]][keys[this.selectedBlock]]);
+                    }
+                    else
+                    {
+                        this.table.set(x, y, null);
                     }
                 }
                 this.lastClick = true;
@@ -157,19 +253,19 @@ class GameModeEdit extends GameMode
             }
             if (this.keyIsDown(sketch, 'up'))
             {
-                this.pos.y -= 2 * time;
+                this.pos.y -= this.speed * time;
             }
             else if (this.keyIsDown(sketch, 'right'))
             {
-                this.pos.x += 2 * time;
+                this.pos.x += this.speed * time;
             }
             else if (this.keyIsDown(sketch, 'down'))
             {
-                this.pos.y += 2 * time;
+                this.pos.y += this.speed * time;
             }
             else if (this.keyIsDown(sketch, 'left'))
             {
-                this.pos.x -= 2 * time;
+                this.pos.x -= this.speed * time;
             }
         }
     }
@@ -213,24 +309,31 @@ class GameModeEdit extends GameMode
     {
         this.scale = scale;
         super.render(sketch, scale, width, height);
-        const offset = {
+        this.offset = {
             x: (width - 4) / 2 - this.pos.x, //center the player on the middle of the map
             y: height / 2 - this.pos.y, //center the player on the middle of the map
         };
         this.x = (width - 4) * scale;
         sketch.push(); //save the curent display of map / player / mob
-        sketch.translate(offset.x * scale, offset.y * scale); //translate all entity and map by offset
+        sketch.translate(this.offset.x * scale, this.offset.y * scale); //translate all entity and map by offset
 
-        const minX = Math.max(0, offset.x - width / 2);
-        const minY = Math.max(0, offset.y - height / 2);
-        const maxX = Math.min(width - offset.x) - 4;
-        const maxY = Math.min(height - offset.y);
+        const minX = Math.max(0, this.offset.x - width / 2);
+        const minY = Math.max(0, this.offset.y - height / 2);
+        const maxX = Math.min(width - this.offset.x) - 4;
+        const maxY = Math.min(height - this.offset.y);
         for (let y = minY; y < maxY; y++)
         {
             for (let x = minX; x < maxX; x++)
             {
                 const tile = this.getTile(x, y);
-                sketch.stroke(255);
+                if (x < this.table.getColumnCount() && y < this.table.getRowCount())
+                {
+                    sketch.stroke(0, 0, 255);
+                }
+                else
+                {
+                    sketch.stroke(255);
+                }
                 sketch.noFill();
                 sketch.square(Math.floor(x) * scale, Math.floor(y) * scale, scale);
                 if (tile !== null)
@@ -244,8 +347,11 @@ class GameModeEdit extends GameMode
         sketch.translate(this.x, 0);
         sketch.fill(0);
         sketch.rect(0, 0, 4 * scale, height * scale);
+        sketch.textAlign(sketch.LEFT, sketch.BOTTOM);
         sketch.fill(255);
-        sketch.text(`selected type : ${this.types[this.selectedType]}`, 10, 10);
+        sketch.textSize(10);
+        sketch.text('Export', 10, 10);
+        sketch.text(`selected type : ${this.types[this.selectedType]}`, 10, 30);
         const keys = Object.keys(Tile.collection[this.types[this.selectedType]]);
         for (let i = 0; i < keys.length; i++)
         {

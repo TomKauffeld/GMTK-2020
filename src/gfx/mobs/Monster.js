72a7f7ded77b28ee351d38b778be112ea68ac04b
Eavydeath <42966059+Eavydeath@users.noreply.github.com>
2020-07-11 14:26:03 +0200

    Void_mob added

--- src/gfx/mobs/Monster.js
+++ src/gfx/mobs/Monster.js
@@ -11,6 +11,7 @@ function load(sketch)
     Monster.forest_mob = TileSet.Create(sketch, '/res/sprites/mobs/monster/02.png', 4);
     Monster.desert_mob = TileSet.Create(sketch, '/res/sprites/mobs/monster/07.png', 4);
     Monster.snow_mob = TileSet.Create(sketch, '/res/sprites/mobs/monster/17.png', 4);
+    Monster.void_mob = TileSet.Create(sketch, '/res/sprites/mobs/monster/16.png', 4);
     return Monster;
 }
 

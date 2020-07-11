1d33134233dcd6aae786a92e7198b8f790de8b06
TomKauffeld <tom.kauffeld@gmail.com>
2020-07-11 17:36:06 +0200

    Update GameItem.js

--- src/game/sprites/entities/GameItem.js
+++ src/game/sprites/entities/GameItem.js
@@ -1,9 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import p5 from 'p5';
-// eslint-disable-next-line no-unused-vars
-import Settings from '../../../Settings';
-// eslint-disable-next-line no-unused-vars
-import World from '../../../World';
+import World from '../../World';
 import Entity from './Entity';
 // import takeRange from './takeRange';
 

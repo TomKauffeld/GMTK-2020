387126958845e74b9d8b136bff34aa596deb6276
TomKauffeld <tom.kauffeld@gmail.com>
2020-07-11 16:04:53 +0200

    Update Monster.js

--- src/game/sprites/entities/mobs/Monster.js
+++ src/game/sprites/entities/mobs/Monster.js
@@ -3,10 +3,7 @@ import p5 from 'p5';
 // eslint-disable-next-line no-unused-vars
 import World from '../../../World';
 import Mob from './Mob';
-<<<<<<< HEAD
 import Player from '../../../../gfx/mobs/Player';
-=======
->>>>>>> master
 
 class Monster extends Mob
 {
@@ -58,15 +55,11 @@ class Monster extends Mob
             this.action.timer = 0;
         }
         this.attack = false;
-<<<<<<< HEAD
         this.speed = 0;
         if(this.inRange(this,Player)){
             this.attack = true;
         }
         else if (this.action.currentAction === 'up')
-=======
-        if (this.action.currentAction === 'up')
->>>>>>> master
         {
             this.setPosD(0);
             this.speed = this.maxSpeed;
@@ -86,18 +79,6 @@ class Monster extends Mob
             this.setPosD(3);
             this.speed = this.maxSpeed;
         }
-<<<<<<< HEAD
-=======
-        else
-        {
-            this.speed = 0;
-        }
-        /*else
-        {
-            this.attack = this.keyIsDown(sketch, 'attack');
-            this.speed = 0;
-        }*/
->>>>>>> master
     }
 
     /**

93f08ef967e825551d4f8355467440d021f45b8b
MaissamB <47453047+MaissamB@users.noreply.github.com>
2020-07-12 17:34:34 +0200

    add help page

--- src/game/modes/GameModeHelp.js
+++ src/game/modes/GameModeHelp.js
@@ -2,6 +2,7 @@
 import {p5InstanceExtensions} from 'p5';
 import GameMode from './GameMode';
 import GameModeMenu from './GameModeMenu';
+import Settings from '../Settings';
 
 
 class GameModeHelp extends GameMode
@@ -35,6 +36,17 @@ class GameModeHelp extends GameMode
     render(sketch, scale, width, height)
     {
         super.render(sketch, scale, width, height);
+        sketch.fill(255,255,255);
+        sketch.textSize(scale);
+        sketch.textAlign(sketch.CENTER, sketch.CENTER);
+        if ( Settings.keys.mode == 'azerty'){
+            sketch.text('Move : z q s d', width/2*scale, height/2*scale-scale*1.5);
+        } 
+        if ( Settings.keys.mode == 'qwerty'){
+            sketch.text('Move : w a s d', width/2*scale, height/2*scale-scale*1.5);
+        } 
+        sketch.text('Attack : SpaceBar', width/2*scale, height/2*scale);    
+        sketch.text('Switch World : Shift', width/2*scale, height/2*scale+scale*1.5);    
     }
 }
 

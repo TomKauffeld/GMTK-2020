9eebfd2942fd95fc8ff6be8366f3faae9765571c
TomKauffeld <tom.kauffeld@gmail.com>
2020-07-12 18:30:12 +0200

    changed buttons

--- src/gfx/buttons/Buttons.js
+++ src/gfx/buttons/Buttons.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import {p5InstanceExtensions} from 'p5';
-import ButtonNone from './ButtonNone';
+import ButtonNone from './ButtonNone';/*
 import ButtonPlay from './ButtonPlay';
-import ButtonSettings from './ButtonSettings';
+import ButtonSettings from './ButtonSettings';*/
 import TileSet from '../TileSet';
 
 /**
@@ -15,8 +15,8 @@ function load(sketch)
         const main = new TileSet(image, 3, 3);
         main.calculate();
         Buttons.none.load(main.getTile(0, 0, 3, 1));
-        Buttons.play.load(main.getTile(0, 1, 3, 1));
-        Buttons.settings.load(main.getTile(0, 2, 3, 1));
+        /*Buttons.play.load(main.getTile(0, 1, 3, 1));
+        Buttons.settings.load(main.getTile(0, 2, 3, 1));*/
     });
     return Buttons;
 }
@@ -25,8 +25,8 @@ function load(sketch)
 const Buttons = {
     load,
     none: ButtonNone,
-    play: ButtonPlay,
-    settings: ButtonSettings
+    /*play: ButtonPlay,
+    /*settings: ButtonSettings*/
 };
 
 

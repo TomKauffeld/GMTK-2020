0a5ee620ec1da73ee8e38352c587bcaea1cec1b8
Tom Kauffeld <tom.kauffeld@gmail.com>
2022-03-11 00:31:10 +0100

    changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-undef */
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'GlitchyLand!',
+        template: path.resolve(__dirname, 'src', 'index.html'),
+      }),
+    ],
     module: {
         rules: [
             {
